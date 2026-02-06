b5cd282a8a60eccc7753a12cc39b91ec04f50246
Максим Бабинцев <bms@arhitru.ru>
2026-02-06 12:12:52 +0300

    5

--- setup_required_packages.sh
+++ setup_required_packages.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+opkg update
 opkg install kmod-leds-gpio kmod odhcp6c odhcpd-ipv6only ppp ppp-mod-pppoe kmod-usb-ledtrig-usbport 
+cd /tmp/ && opkg download dnsmasq-full
+opkg remove dnsmasq && opkg install dnsmasq-full --cache /tmp/
+mv /etc/config/dhcp-opkg /etc/config/dhcp
+
+opkg install curl
