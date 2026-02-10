5ecf3738584f42f1ae1dcd7126ea7bf48c6e123a
Максим Бабинцев <bms@arhitru.ru>
2026-02-10 14:20:01 +0300

    Update setup_required_packages.sh

--- setup_required_packages.sh
+++ setup_required_packages.sh
@@ -33,6 +33,79 @@ while true; do
         *) echo "Please answer y or n.";;
     esac
 done
+if [ "$TUN" = "y" ] || [ "$TUN" = "Y" ]; then
+    export TUNNEL="tun2socks"
+    # Считывает пользовательскую переменную для конфигурации Outline (Shadowsocks)
+    read -p "Enter Outline (Shadowsocks) Config (format ss://base64coded@HOST:PORT/?outline=1): " OUTLINECONF
+    export  OUTLINECONF=$OUTLINECONF
+    echo "Configure DNSCrypt2 or Stubby? It does matter if your ISP is spoofing DNS requests"
+    echo "Select:"
+    echo "1) No [Default]"
+    echo "2) DNSCrypt2 (10.7M)"
+    echo "3) Stubby (36K)"
+
+    while true; do
+    read -r -p '' DNS_RESOLVER
+        case $DNS_RESOLVER in 
+
+        1) 
+            echo "Skiped"
+            break
+            ;;
+
+        2)
+            export DNS_RESOLVER="DNSCRYPT"
+            break
+            ;;
+
+        3) 
+            export DNS_RESOLVER="STUBBY"
+            break
+            ;;
+
+        *)
+            echo "Choose from the following options"
+            ;;
+        esac
+    done
+    echo "Choose you country"
+    echo "Select:"
+    echo "1) Russia inside. You are inside Russia"
+    echo "2) Russia outside. You are outside of Russia, but you need access to Russian resources"
+    echo "3) Ukraine. uablacklist.net list"
+    echo "4) Skip script creation"
+
+    while true; do
+    read -r -p '' COUNTRY
+        case $COUNTRY in 
+
+        1) 
+            export COUNTRY="russia_inside"
+            break
+            ;;
+
+        2)
+            export COUNTRY="russia_outside"
+            break
+            ;;
+
+        3) 
+            export COUNTRY="ukraine"
+            break
+            ;;
+
+        4) 
+            echo "Skiped"
+            export COUNTRY=0
+            break
+            ;;
+
+        *)
+            echo "Choose from the following options"
+            ;;
+        esac
+    done
+fi
 
 ## **Сохранение списков программных пакетов при загрузке**
 # Сохранение статуса установленных пакетов opkg в /usr/lib/opkg/lists хранящемся в extroot, а не в RAM, экономит некоторую оперативную память и сохраняет списки пакетов доступными после перезагрузки.
