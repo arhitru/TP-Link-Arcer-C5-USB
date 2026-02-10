a3cb8cd09203d46f39ffbe5e710d28e8e69e9163
Максим Бабинцев <bms@arhitru.ru>
2026-02-10 16:23:11 +0300

    Create setup_all.sh

---
+++ setup_all.sh
@@ -0,0 +1,2 @@
+cd /tmp && wget https://raw.githubusercontent.com/arhitru/TP-Link-Arcer-C5-USB/main/mount_usb.sh -O mount_usb.sh && chmod +x mount_usb.sh && ./mount_usb.sh
+cd /tmp && wget https://raw.githubusercontent.com/arhitru/TP-Link-Arcer-C5-USB/main/setup_required_packages.sh -O setup_required_packages.sh && chmod +x setup_required_packages.sh && ./setup_required_packages.sh
