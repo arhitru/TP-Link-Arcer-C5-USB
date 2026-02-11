dcbb61406b9c1f7170b544ec22fc291759a20cdd
Максим Бабинцев <bms@arhitru.ru>
2026-02-11 08:45:18 +0300

    Delete setup_all.sh

--- setup_all.sh
+++
@@ -1,2 +0,0 @@
-cd /tmp && wget https://raw.githubusercontent.com/arhitru/TP-Link-Arcer-C5-USB/main/mount_usb.sh -O mount_usb.sh && chmod +x mount_usb.sh && ./mount_usb.sh
-cd /tmp && wget https://raw.githubusercontent.com/arhitru/TP-Link-Arcer-C5-USB/main/setup_required_packages.sh -O setup_required_packages.sh && chmod +x setup_required_packages.sh && ./setup_required_packages.sh
