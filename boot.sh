0b2c18128bbc05733a6b080c7f288f29525cf99d
Максим Бабинцев <bms@arhitru.ru>
2026-02-12 14:54:36 +0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 LOG="/root/boot.log"
+test_hosts="openwrt.org google.com cloudflare.com"
 echo "=== Начало установки: $(date) ===" > $LOG
 
 echo "Проверка системы:" >> $LOG
@@ -11,9 +12,15 @@ echo "Ожидание сети..." >> $LOG
 for i in $(seq 1 120); do
     if ping -c 1 -W 1 8.8.8.8 >/dev/null 2>&1; then
         echo "Сеть доступна"  >> $LOG
-        break
+        for host in $test_hosts; do
+            if ping -c 1 -W 3 "$host" >/dev/null 2>&1; then
+                log_info "Подключение к $host успешно"
+                break
+            fi
+        done
     fi
     sleep 1
 done
+
 echo "Loading..." >> $LOG
 cd /root && wget https://raw.githubusercontent.com/arhitru/TP-Link-Arcer-C5-USB/main/setup.sh >> $LOG 2>&1 && chmod +x setup.sh && ./setup.sh
