d82c0b073b0362873e2d243c279f40ee15c96616
Максим Бабинцев <bms@arhitru.ru>
2026-02-18 12:02:13 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -82,144 +82,144 @@ if [ -t 0 ]; then
         fi
         ./get_outline_settings.sh
 
-        if [ "$TUN" = "y" ] || [ "$TUN" = "Y" ]; then
-            export TUNNEL="tun2socks"
-            # Проверка версии OpenWrt
-            if [ -f /etc/os-release ]; then
-                # shellcheck source=/etc/os-release
-                . /etc/os-release
-                log_info "Версия OpenWrt: $OPENWRT_RELEASE"
+#         if [ "$TUN" = "y" ] || [ "$TUN" = "Y" ]; then
+#             export TUNNEL="tun2socks"
+#             # Проверка версии OpenWrt
+#             if [ -f /etc/os-release ]; then
+#                 # shellcheck source=/etc/os-release
+#                 . /etc/os-release
+#                 log_info "Версия OpenWrt: $OPENWRT_RELEASE"
                 
-                VERSION=$(grep 'VERSION=' /etc/os-release | cut -d'"' -f2)
-                VERSION_ID=$(echo "$VERSION" | awk -F. '{print $1}')
-                export VERSION_ID
+#                 VERSION=$(grep 'VERSION=' /etc/os-release | cut -d'"' -f2)
+#                 VERSION_ID=$(echo "$VERSION" | awk -F. '{print $1}')
+#                 export VERSION_ID
                 
-                # Проверка совместимости
-                if [ "$VERSION_ID" -lt 19 ]; then
-                    log_warn "Версия OpenWrt ($VERSION_ID) может быть несовместима"
-                fi
-            else
-                VERSION_ID=0
-                log_warn "Не удалось определить версию OpenWrt"
-            fi
+#                 # Проверка совместимости
+#                 if [ "$VERSION_ID" -lt 19 ]; then
+#                     log_warn "Версия OpenWrt ($VERSION_ID) может быть несовместима"
+#                 fi
+#             else
+#                 VERSION_ID=0
+#                 log_warn "Не удалось определить версию OpenWrt"
+#             fi
 
-            # Считывает пользовательскую переменную для конфигурации Outline (Shadowsocks)
-            read -p "Enter Outline (Shadowsocks) Config (format ss://base64coded@HOST:PORT/?outline=1): " OUTLINECONF
-            export  OUTLINECONF=$OUTLINECONF
+#             # Считывает пользовательскую переменную для конфигурации Outline (Shadowsocks)
+#             read -p "Enter Outline (Shadowsocks) Config (format ss://base64coded@HOST:PORT/?outline=1): " OUTLINECONF
+#             export  OUTLINECONF=$OUTLINECONF
 
-            printf "\033[33mConfigure DNSCrypt2 or Stubby? It does matter if your ISP is spoofing DNS requests\033[0m\n"
-            echo "Select:"
-            echo "1) No [Default]"
-            echo "2) DNSCrypt2 (10.7M)"
-            echo "3) Stubby (36K)"
+#             printf "\033[33mConfigure DNSCrypt2 or Stubby? It does matter if your ISP is spoofing DNS requests\033[0m\n"
+#             echo "Select:"
+#             echo "1) No [Default]"
+#             echo "2) DNSCrypt2 (10.7M)"
+#             echo "3) Stubby (36K)"
 
-            while true; do
-            read -r -p '' DNS_RESOLVER
-                case $DNS_RESOLVER in 
+#             while true; do
+#             read -r -p '' DNS_RESOLVER
+#                 case $DNS_RESOLVER in 
 
-                1) 
-                    echo "Skiped"
-                    break
-                    ;;
+#                 1) 
+#                     echo "Skiped"
+#                     break
+#                     ;;
 
-                2)
-                    export DNS_RESOLVER="DNSCRYPT"
-                    break
-                    ;;
+#                 2)
+#                     export DNS_RESOLVER="DNSCRYPT"
+#                     break
+#                     ;;
 
-                3) 
-                    export DNS_RESOLVER="STUBBY"
-                    break
-                    ;;
+#                 3) 
+#                     export DNS_RESOLVER="STUBBY"
+#                     break
+#                     ;;
 
-                *)
-                    echo "Choose from the following options"
-                    ;;
-                esac
-            done
+#                 *)
+#                     echo "Choose from the following options"
+#                     ;;
+#                 esac
+#             done
 
-            printf "\033[33mChoose you country\033[0m\n"
-            echo "Select:"
-            echo "1) Russia inside. You are inside Russia"
-            echo "2) Russia outside. You are outside of Russia, but you need access to Russian resources"
-            echo "3) Ukraine. uablacklist.net list"
-            echo "4) Skip script creation"
+#             printf "\033[33mChoose you country\033[0m\n"
+#             echo "Select:"
+#             echo "1) Russia inside. You are inside Russia"
+#             echo "2) Russia outside. You are outside of Russia, but you need access to Russian resources"
+#             echo "3) Ukraine. uablacklist.net list"
+#             echo "4) Skip script creation"
 
-            while true; do
-            read -r -p '' COUNTRY
-                case $COUNTRY in 
+#             while true; do
+#             read -r -p '' COUNTRY
+#                 case $COUNTRY in 
 
-                1) 
-                    export COUNTRY="russia_inside"
-                    break
-                    ;;
+#                 1) 
+#                     export COUNTRY="russia_inside"
+#                     break
+#                     ;;
 
-                2)
-                    export COUNTRY="russia_outside"
-                    break
-                    ;;
+#                 2)
+#                     export COUNTRY="russia_outside"
+#                     break
+#                     ;;
 
-                3) 
-                    export COUNTRY="ukraine"
-                    break
-                    ;;
+#                 3) 
+#                     export COUNTRY="ukraine"
+#                     break
+#                     ;;
 
-                4) 
-                    echo "Skiped"
-                    export COUNTRY=0
-                    break
-                    ;;
+#                 4) 
+#                     echo "Skiped"
+#                     export COUNTRY=0
+#                     break
+#                     ;;
 
-                *)
-                    echo "Choose from the following options"
-                    ;;
-                esac
-            done
-            # Ask user to use Outline as default gateway
-            # Задает вопрос пользователю о том, следует ли использовать Outline в качестве шлюза по умолчанию
-            read -p "Use Outline as default gateway? [y/N]: " DEFAULT_GATEWAY
-            if [ "$DEFAULT_GATEWAY" = "y" ] || [ "$DEFAULT_GATEWAY" = "Y" ]; then
-                export OUTLINE_DEFAULT_GATEWAY=$DEFAULT_GATEWAY
-            fi
-            if [ ! -f "$OUTLINE_CONFIG_FILE" ]; then
-                echo "Файл конфигурации Outline" | tee -a $LOG
-                cat > "$OUTLINE_CONFIG_FILE" << 'EOF'
-# ============================================================================
-# Конфигурация outline_vpn
-# ============================================================================
+#                 *)
+#                     echo "Choose from the following options"
+#                     ;;
+#                 esac
+#             done
+#             # Ask user to use Outline as default gateway
+#             # Задает вопрос пользователю о том, следует ли использовать Outline в качестве шлюза по умолчанию
+#             read -p "Use Outline as default gateway? [y/N]: " DEFAULT_GATEWAY
+#             if [ "$DEFAULT_GATEWAY" = "y" ] || [ "$DEFAULT_GATEWAY" = "Y" ]; then
+#                 export OUTLINE_DEFAULT_GATEWAY=$DEFAULT_GATEWAY
+#             fi
+#             if [ ! -f "$OUTLINE_CONFIG_FILE" ]; then
+#                 echo "Файл конфигурации Outline" | tee -a $LOG
+#                 cat > "$OUTLINE_CONFIG_FILE" << 'EOF'
+# # ============================================================================
+# # Конфигурация outline_vpn
+# # ============================================================================
 
-TUNNEL="tun2socks"
-OUTLINECONF=$OUTLINECONF
-DNS_RESOLVER=$DNS_RESOLVER
-COUNTRY=$COUNTRY
-OUTLINE_DEFAULT_GATEWAY=$DEFAULT_GATEWAY
-VERSION_ID=$VERSION_ID
+# TUNNEL="tun2socks"
+# OUTLINECONF=$OUTLINECONF
+# DNS_RESOLVER=$DNS_RESOLVER
+# COUNTRY=$COUNTRY
+# OUTLINE_DEFAULT_GATEWAY=$DEFAULT_GATEWAY
+# VERSION_ID=$VERSION_ID
 
-# Список обязательных пакетов
-REQUIRED_PACKAGES="
-curl
-nano
-kmod-tun
-ip-full
-"
+# # Список обязательных пакетов
+# REQUIRED_PACKAGES="
+# curl
+# nano
+# kmod-tun
+# ip-full
+# "
 
-# Пакеты для замены
-REPLACE_PACKAGES="
-dnsmasq:dnsmasq-full
-"
+# # Пакеты для замены
+# REPLACE_PACKAGES="
+# dnsmasq:dnsmasq-full
+# "
 
-# Таймаут для операций (секунды)
-OPKG_TIMEOUT=300
+# # Таймаут для операций (секунды)
+# OPKG_TIMEOUT=300
 
-# Количество попыток при ошибке
-RETRY_COUNT=3
+# # Количество попыток при ошибке
+# RETRY_COUNT=3
 
-# Режим отладки (0/1)
-DEBUG=0
-EOF
-                echo "Создан файл конфигурации по умолчанию: $OUTLINE_CONFIG_FILE" | tee -a $LOG
-            fi
-        fi
+# # Режим отладки (0/1)
+# DEBUG=0
+# EOF
+#                 echo "Создан файл конфигурации по умолчанию: $OUTLINE_CONFIG_FILE" | tee -a $LOG
+#             fi
+#         fi
     fi
 fi
 
