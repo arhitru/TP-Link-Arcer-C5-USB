d28aba3b8b0f8e4b7676b4d3e539fd7f1232fab9
Максим Бабинцев <bms@arhitru.ru>
2026-02-18 09:23:50 +0300

    Update mount_usb.sh

--- mount_usb.sh
+++ mount_usb.sh
@@ -26,7 +26,7 @@ fi
 init_logging
 
 #  Импорт функций настройки USB
-if [ ! -f "/root/mount_usb.sh" ]; then
+if [ ! -f "/root/mount_usb_function.sh" ]; then
     cd /root && wget https://raw.githubusercontent.com/arhitru/TP-Link-Arcer-C5-USB/main/mount_usb_function.sh >> $LOG_FILE 2>&1 && chmod +x mount_usb_function.sh
 fi
 . /root/mount_usb_function.sh
