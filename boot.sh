d441d34f13a53e177964f12141e0d86db0d2d1be
Максим Бабинцев <bms@arhitru.ru>
2026-02-27 09:20:25 +0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -3,3 +3,5 @@
 sleep 150
 
 cd /root && wget https://raw.githubusercontent.com/arhitru/TP-Link-Arcer-C5-USB/main/setup.sh >> $LOG 2>&1 && chmod +x setup.sh && ./setup.sh
+
+# base-files dnsmasq dropbear fstools libc libgcc mtd netifd opkg procd-ujail uci uclient-fetch kmod-usb2 kmod-switch-rtl8367b block-mount kmod-fs-ext4 e2fsprogs parted kmod-usb-storage
